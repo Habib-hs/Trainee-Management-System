bd5efdc33c104e73346257d80dc10b13496a83c1
Habibullah Howlader <habibullah.howlader@bjitacademy.com>
2023-07-13 09:53:52 +0600

    fixed trainee assignment validation problem

--- src/main/java/com/backend/tms/service/Impl/AssignTraineeServiceImp.java
+++ src/main/java/com/backend/tms/service/Impl/AssignTraineeServiceImp.java
@@ -2,8 +2,10 @@ package com.backend.tms.service.Impl;
 
 import com.backend.tms.entity.BatchEntity;
 import com.backend.tms.entity.TraineeEntity;
+import com.backend.tms.entity.TrainerEntity;
 import com.backend.tms.exception.custom.BatchNotFoundException;
 import com.backend.tms.exception.custom.TraineeNotFoundException;
+import com.backend.tms.exception.custom.TrainerNotFoundException;
 import com.backend.tms.repository.BatchRepository;
 import com.backend.tms.repository.TraineeRepository;
 import com.backend.tms.service.AssignTraineeService;
@@ -12,7 +14,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -25,15 +29,28 @@ public class AssignTraineeServiceImp implements AssignTraineeService {
     public ResponseEntity<Object> addTraineesToBatch(Long batchId, List<Long> traineeIds) {
         BatchEntity batch = batchRepository.findById(batchId).orElseThrow(() -> new BatchNotFoundException("Batch not found"));
 
-        for (Long traineeId : traineeIds) {
-            var trainee =traineeRepository.findById(traineeId);
-            if(trainee.isPresent()){
-                batch.getTrainees().add(trainee.get());
-            }else{
-                System.out.println("No batch found with id"+ traineeId);
-            }
+
+        Set<TraineeEntity> trainee = new HashSet<>(traineeRepository.findAllById(traineeIds));
+        if (trainee.size() == 0) {
+            throw new TraineeNotFoundException("No trainee found for the provided IDs");
         }
-        batchRepository.save(batch);
+
+        //need to add here if a trainer exist in same or another batch.
+        // the trainee that are not assigned in a batch earlier keep them in List<Long> traineeIds
+        //the trainee that are exist another batch keep them in a list name existed trainee
+
+
+        //all the trainees are not present in db don't save them
+        if (traineeIds.size() != trainee.size()) {
+            int diff = traineeIds.size() - trainee.size();
+            batch.getTrainees().addAll(trainee);
+            batchRepository.save(batch);
+            String responseMessage = diff + " trainee IDs you provided are not valid. Only valid trainee are added to the Batch";
+            return new ResponseEntity<>(responseMessage, HttpStatus.OK);
+        }else{
+            batch.getTrainees().addAll(trainee);
+            batchRepository.save(batch);
+        }
 
         // Return a success message
         return new ResponseEntity<>("Assign Trainees successfully", HttpStatus.OK);
--- src/main/java/com/backend/tms/service/Impl/AssignTrainerServiceImp.java
+++ src/main/java/com/backend/tms/service/Impl/AssignTrainerServiceImp.java
@@ -3,6 +3,7 @@ package com.backend.tms.service.Impl;
 import com.backend.tms.entity.BatchEntity;
 import com.backend.tms.entity.TrainerEntity;
 import com.backend.tms.exception.custom.BatchNotFoundException;
+import com.backend.tms.exception.custom.TrainerNotFoundException;
 import com.backend.tms.repository.BatchRepository;
 import com.backend.tms.repository.TrainerRepository;
 import com.backend.tms.service.AssignTrainerService;
@@ -28,8 +29,22 @@ public class AssignTrainerServiceImp implements AssignTrainerService {
                     .orElseThrow(() -> new BatchNotFoundException("Batch not found"));
 
         Set<TrainerEntity> trainers = new HashSet<>(trainerRepository.findAllById(trainerIds));
-        batchEntity.getTrainers().addAll(trainers);
+        if (trainers.size() == 0) {
+            throw new TrainerNotFoundException("No trainers found for the provided IDs");
+        }
 
+        //all the trainers are not present in db
+            if (trainerIds.size() != trainers.size()) {
+                int diff = trainerIds.size() - trainers.size();
+                batchEntity.getTrainers().addAll(trainers);
+                batchRepository.save(batchEntity);
+                String responseMessage = diff + " trainers IDs you provided are not valid. Only valid trainers are added to the Batch";
+                return new ResponseEntity<>(responseMessage, HttpStatus.OK);
+            }
+
+
+            //all the trainers are valid
+        batchEntity.getTrainers().addAll(trainers);
         batchRepository.save(batchEntity);
 
         // Return a success message
