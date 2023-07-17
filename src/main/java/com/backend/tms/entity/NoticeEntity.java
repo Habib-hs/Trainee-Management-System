599f2b6216b4d3edaf8ab2700d038648d6617f30
Habibullah Howlader <habibullah.howlader@bjitacademy.com>
2023-07-17 09:13:12 +0600

    notice entity updated

--- src/main/java/com/backend/tms/entity/NoticeEntity.java
+++ src/main/java/com/backend/tms/entity/NoticeEntity.java
@@ -1,7 +1,6 @@
 package com.backend.tms.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,6 +13,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class NoticeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String NoticeTitle;
     private Long BatchId;
