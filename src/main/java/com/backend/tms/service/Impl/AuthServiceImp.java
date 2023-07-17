873824618dc2102878875c6d7ab1bab27d53ce4e
Habibullah Howlader <habibullah.howlader@bjitacademy.com>
2023-07-17 14:29:02 +0600

    updated trainer and trainee registration Implementation

--- src/main/java/com/backend/tms/service/Impl/AuthServiceImp.java
+++ src/main/java/com/backend/tms/service/Impl/AuthServiceImp.java
@@ -5,15 +5,12 @@ import com.backend.tms.entity.TraineeEntity;
 import com.backend.tms.entity.TrainerEntity;
 import com.backend.tms.entity.UserEntity;
 import com.backend.tms.exception.custom.TraineeAlreadyExistsException;
-import com.backend.tms.mapper.AuthMapper;
 import com.backend.tms.model.Admin.AdminReqModel;
 import com.backend.tms.model.Admin.AdminResModel;
 import com.backend.tms.model.Common.AuthenticationReqModel;
 import com.backend.tms.model.Common.AuthenticationResModel;
 import com.backend.tms.model.Trainee.TraineeReqModel;
-import com.backend.tms.model.Trainee.TraineeResModel;
 import com.backend.tms.model.Trainer.TrainerReqModel;
-import com.backend.tms.model.Trainer.TrainerResModel;
 import com.backend.tms.repository.AdminRepository;
 import com.backend.tms.repository.TraineeRepository;
 import com.backend.tms.repository.TrainerRepository;
@@ -21,6 +18,7 @@ import com.backend.tms.repository.UserRepository;
 import com.backend.tms.service.AuthService;
 import com.backend.tms.utlis.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -39,6 +37,7 @@ public class AuthServiceImp implements AuthService {
     private final AdminRepository adminRepository;
     private final TraineeRepository traineeRepository;
     private final TrainerRepository trainerRepository;
+    private final ModelMapper modelMapper;
 
     @Override
     public ResponseEntity<Object> registerAdmin(AdminReqModel adminModel) {
@@ -60,7 +59,7 @@ public class AuthServiceImp implements AuthService {
         UserEntity newUser = userRepository.save(userEntity);
 
         //mapping the admin
-        AdminEntity admin = AuthMapper.adminReqModeltoEntity(adminModel);
+        AdminEntity admin = modelMapper.map(adminModel, AdminEntity.class);
         admin.setUser(newUser); // Set the UserEntity as the associated user
 
         // Save the AdminEntity
@@ -71,7 +70,6 @@ public class AuthServiceImp implements AuthService {
 
         // Return the AdminResModel as the response
         return new ResponseEntity<>(adminResModel, HttpStatus.OK);
-
 
     }
 
@@ -94,19 +92,11 @@ public class AuthServiceImp implements AuthService {
         // Save the UserEntity to generate the ID
         UserEntity newUser = userRepository.save(userTrainee);
 
-
         // Map TraineeReqModel to TraineeEntity
-        TraineeEntity trainee = AuthMapper.traineeReqModeltoEntity(traineeModel);
-        trainee.setUser(newUser); // Set the UserEntity as the associated user
-
-        // Save the TraineeEntity
+        TraineeEntity trainee = modelMapper.map(traineeModel, TraineeEntity.class);
+        trainee.setUser(newUser);
         traineeRepository.save(trainee);
-
-        // Map the saved TraineeEntity to TraineeResModel
-        TraineeResModel traineeResModel = AuthMapper.traineeEntityResModel(trainee);
-
-        // Return the TraineeResModel as the response
-        return new ResponseEntity<>(traineeResModel, HttpStatus.OK);
+        return new ResponseEntity<>("Trainee Created Successfully", HttpStatus.OK);
     }
 
     @Override
@@ -129,14 +119,10 @@ public class AuthServiceImp implements AuthService {
         UserEntity newUser = userRepository.save(userEntity);
 
         // Map TrainerReqModel to TrainerEntity
-        TrainerEntity trainer = AuthMapper.trainerReqModeltoEntity(trainerModel);
+        TrainerEntity trainer = modelMapper.map(trainerModel, TrainerEntity.class);
         trainer.setUser(newUser);
-        // Save the TrainerEntity
         trainerRepository.save(trainer);
-        TrainerResModel trainerResModel = AuthMapper.trainerEntityResModel(trainer);
-
-        // Return the TrainerResModel as the response
-        return new ResponseEntity<>(trainerResModel, HttpStatus.OK);
+        return new ResponseEntity<>("trainer created successfully", HttpStatus.OK);
 
     }
 
