b00fc7bf2a101f1b7ff211d16864958672805ffe
Habibullah Howlader <habibullah.howlader@bjitacademy.com>
2023-07-18 12:13:45 +0600

    FileService Class added in Utlis

---
+++ src/main/java/com/backend/tms/utlis/FileService.java
@@ -0,0 +1,27 @@
+package com.backend.tms.utlis;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileService {
+    private String uploadFile(MultipartFile file, String UPLOAD_DIR) {
+        if (file != null && !file.isEmpty()) {
+            try {
+                String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+                File destinationDir = new File(UPLOAD_DIR);
+                if (!destinationDir.exists()) {
+                    destinationDir.mkdirs(); // Create the directory if it doesn't exist
+                }
+                File destinationFile = new File(destinationDir, fileName);
+                file.transferTo(destinationFile);
+                return destinationFile.getAbsolutePath();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+}
